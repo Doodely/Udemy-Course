be373dfac3c2c2d52b848d6f3d154e70aa90e088
Doodley <mpyrtle.dccc@gmail.com>
2018-02-18 18:12:51 -0500

    Arrow function declaration

---
+++ ts-hello/arrow-func.js
@@ -0,0 +1,8 @@
+// Long way to define a function
+var log = function (message) {
+    console.log(message);
+};
+// Shorter Way (One-line code can ommit {})
+var doLog = function (message) { return console.log(message); }; // C# called LAMDA; Called Arrow in TypeScript
+// No parameters
+var doEmptyLog = function () { return console.log(); };
