758cafe1faa29fb8bac267f6097ec41453389eb3
Doodley <mpyrtle.dccc@gmail.com>
2018-02-18 20:17:11 -0500

    {{}} Declarator for template to data-bind

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -19,8 +19,13 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'courses', // CSS selector: <courses>
-    template: '<h2>Courses</h2>' // Can be much more advanced than this...
+    selector: 'courses',
+    template: '<h2>{{ getTitle() }}</h2>' // Passing data; data-binding. Can pass functions / variables, etc
 })
 export class CoursesComponent {
+    title = 'List of courses';
+
+    getTitle() {
+        return "Title: " + this.title;
+    }
 }
