18c9210a7b6fe1cb0725a3480bb4d2a43f253d65
Doodley <mpyrtle.dccc@gmail.com>
2018-02-18 19:35:54 -0500

    Made properties read-only to prevent overrides

--- ts-hello/exercise/like.component.ts
+++ ts-hello/exercise/like.component.ts
@@ -1,10 +1,19 @@
 export class LikeComponent {
-    constructor(public likesCount: number, public isSelected: boolean) {
+    // Properties
+    get likesCount() {
+        return this._likesCount;
+    }
+
+    get isSelected() {
+        return this._isSelected;
+    }
+
+    constructor(private _likesCount: number, private _isSelected: boolean) {
     }
 
     // Click function
     onClick() {
-        this.likesCount += (this.isSelected) ? 1 : -1;
-        this.isSelected = !this.isSelected;
+        this._likesCount += (this._isSelected) ? 1 : -1;
+        this._isSelected = !this._isSelected;
     }
 }
