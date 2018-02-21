5814d8aa65f60c500e81f45a926282d0a509aeab
Doodley <mpyrtle.dccc@gmail.com>
2018-02-20 19:46:12 -0500

    Class binding example

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core'
 import { CoursesService } from './courses.service';
 
+/*
+    Example of a class-binding (property bind)
+        - Can help with toggling classes that aren't always required
+*/
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary">Save</button>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
+    isActive = false;
 }
