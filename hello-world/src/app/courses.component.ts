50f207f65a623938e070de4265e9c99db235c93b
Doodley <mpyrtle.dccc@gmail.com>
2018-02-20 20:17:54 -0500

    Two-Way Bindings and import of FormsModule

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,16 +1,36 @@
 /*
-    Template Variables:
-        - Instead of passing the $event object around, Angular has a way to reference
-        the input field through a variable
+    Two-Way Binding:
+        - It kind of sucks to pass the email object around, and could definitely be 
+        better simplified and more practical:
+            + email; field is used to encapsulate the data
+            + onKeyUp method represents the logic behind the view
+        
+        - If we initialize the email field and then modify it on the front-end, 
+        the console log will only show the initial value and not the updated. This 
+        is a one-way binding.
 
-        Example being an email capture:
-            <input #email
-
-            - The # represents a template variable reference
-            - You can then call .value or other properties of JS from the referenced variable 
-            like you would with typical variables!
-
+            <input [value]="email" (keyup.enter)="onKeyUp()"/>
             
+            export class CoursesComponent { email = "me@example.com"; }
+
+            + Remember: Property-binding goes from component to view; one-way.
+        
+        - Making it two-way:
+
+            // Bad way -- Setting value and then executing the method
+            <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()"/>
+
+            // Good Way -- Syntax for two-way binding            
+            <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+            + Instead of property-binding []; use [(ngModel)] (Banana in a box)
+            + ngModel belongs to the Angular Core for Forms which needs to be imported:
+
+                app.module.ts
+                
+                    import { FormsModule } from '@angular/forms';
+
+                    imports: [ FormsModule ]
+        
 */
 import { Component } from '@angular/core'
 import { CoursesService } from './courses.service';
@@ -18,11 +38,16 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-        <input #email (keyup.enter)="onKeyUp(email.value)"/>
+        <!--<input [value]="email" (keyup.enter)="onKeyUp()"/>-->
+        <!--<input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()"/>-->
+        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
+
     `
 })
 export class CoursesComponent {
-    onKeyUp(email) {
-        console.log(email);
+    email = "me@example.com";
+
+    onKeyUp() {
+        console.log(this.email);
     }
 }
