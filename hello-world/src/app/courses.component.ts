9321396a5d58e66316bdc2b52582e07841bac125
Doodley <mpyrtle.dccc@gmail.com>
2018-02-20 19:48:30 -0500

    Styling binding; expanding on class binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,13 +4,16 @@ import { CoursesService } from './courses.service';
 /*
     Example of a class-binding (property bind)
         - Can help with toggling classes that aren't always required
+
+    Example of style-binding (attribute binding)
+        - List of all applicable: https://www.w3schools.com/jsref/dom_obj_style.asp
 */
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
     `
 })
 export class CoursesComponent {
-    isActive = false;
+    isActive = true;
 }
