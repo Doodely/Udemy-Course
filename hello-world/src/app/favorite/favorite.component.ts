a4a4ef6fe2e4e819740558993caffd724b801b3a
Doodley <mpyrtle.dccc@gmail.com>
2018-02-26 21:25:33 -0500

    Styles use

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,26 +1,25 @@
+/*
+  Three ways to utilize styles:
+    - styleUrls: ['']
+    - styles: [] using ` ` to write styles inline (smaller styles)
+      + Angular will choose the one that comes last.
+    - inline in HTML template
+*/
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-
-  /* 
-    Template Property (Can only choose templateUrl or template; NOT both)
-    Ideally you use template for less than 5-10 lines worth of code; else 
-    reference a templateUrl
-
-    template: '',
-  */
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  styles: [
+    `
+    .fa-star.blue { color: green; }      
+    `
+  ]
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   @Input('is-favorite') isFavorite: boolean;
   @Output('change') click = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onClick() {
     // Toggle
