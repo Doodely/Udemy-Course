c9949350b7ed3b724c5a5a4a949032de9048d59f
Doodley <mpyrtle.dccc@gmail.com>
2018-02-26 20:52:39 -0500

    Defining Input Properties

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
+  //inputs: ['isFavorite']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
+  /*
+    Two ways to mark this as an input property:
+      - Add "Input" to the import and then annotate the field @Input()
+          + This will expose the field
+      - Declare the field as an input field in component metadata
+        + inputs: ['']
+          ++ This will control all fields that could be declared as inputs
+          ++ Limited by requiring the field to be present (so renaming or other refactoring)
+          could compromise code with false data or weird effects.
+          ++ Might be better to use the first approach.
+  */
+  //isFavorite: boolean;
+  @Input() isFavorite: boolean;
 
   constructor() { }
 
