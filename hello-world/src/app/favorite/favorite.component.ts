7f4423ef97246cc7e4252b22ecdc564357c186ea
Doodley <mpyrtle.dccc@gmail.com>
2018-02-26 21:04:22 -0500

    Output properties and EventEmitter

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,26 +1,21 @@
-//import { Component, OnInit, Input } from '@angular/core';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
-  //inputs: ['isFavorite']
 })
 export class FavoriteComponent implements OnInit {
-  /*
-    Two ways to mark this as an input property:
-      - Add "Input" to the import and then annotate the field @Input()
-          + This will expose the field
-      - Declare the field as an input field in component metadata
-        + inputs: ['']
-          ++ This will control all fields that could be declared as inputs
-          ++ Limited by requiring the field to be present (so renaming or other refactoring)
-          could compromise code with false data or weird effects.
-          ++ Might be better to use the first approach.
-  */
-  //isFavorite: boolean;
   @Input('is-favorite') isFavorite: boolean;
+
+  /*
+    - Imported the Output class
+    - Declared the output for the type of event and set it to 
+    the non-primative type "EventEmitter"
+      + Add to imports from Core
+      + .emit() passes along the information that the event fired.
+  */
+  @Output() change = new EventEmitter();
 
   constructor() { }
 
@@ -30,5 +25,6 @@ export class FavoriteComponent implements OnInit {
   onClick() {
     // Toggle
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
 }
