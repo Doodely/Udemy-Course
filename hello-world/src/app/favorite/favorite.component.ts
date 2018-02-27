6ecacda8a43e666d3be0ca1f06f5edd91d638235
Doodley <mpyrtle.dccc@gmail.com>
2018-02-26 21:19:28 -0500

    Information on template vs templateUrl; networking on bundling

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -3,12 +3,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
+
+  /* 
+    Template Property (Can only choose templateUrl or template; NOT both)
+    Ideally you use template for less than 5-10 lines worth of code; else 
+    reference a templateUrl
+
+    template: '',
+  */
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
   @Input('is-favorite') isFavorite: boolean;
-  
-  // Output alias; like Input
   @Output('change') click = new EventEmitter();
 
   constructor() { }
